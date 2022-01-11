e3e3a568d1c489b60156ea5d448847088cfa4278
CUI <garycui222@gmail.com>
2022-01-11 01:23:56 -0500

    set up Routes '/maps/:id'  with fake datebse, still need to test

--- db_service/db_helper.js
+++ db_service/db_helper.js
@@ -26,9 +26,86 @@ const getAllUsers = function(db) {
 };
 
 
+const getPointsByMapId = function(db, mapID) {
+  // still need to test when db set up
+
+  // let mapValues = [mapID];
+  // let queryString = `SELECT * FROM points
+  //                       WHERE map_id = $1`;
+  // return (
+  //   db
+  //     .query(queryString, mapValues)
+  //     .then(res => {
+  //       return res.rows;
+  //     })
+  //     .catch(err => console.log(err))
+  // );
+
+  //dummy data for now
+  let dbRes = [
+    {id: 1,
+      name: 'toronto',
+      latitude: 45.5017,
+      longitude: -73.5673,
+      map_id:mapID
+    }
+  ];
+  return Promise.resolve(dbRes);
+};
+
+const addMap = function(db, mapInfo) {
+  // still need to test when db set up
+
+  // let mapValues = [mapInfo.name, mapInfo.latitude, mapInfo.longitude];
+  // let queryString = `INSERT INTO maps (name, latitude, longitude)
+  // VALUES ($1,$2,$3) RETURNING *;`;
+  // return db
+  //   .query(queryString, mapValues)
+  //   .then(res => res.rows[0])
+  //   .catch(err => console.log(err));
+  let dbRes = [
+    {id: 2,
+      name: 'Markham',
+      latitude: 46,
+      longitude: -60,
+    }
+  ];
+  return Promise.resolve(dbRes);
+
+};
+
+const editMap = function(db, mapInfo) {
+  // still need to test when db set up
+
+  // let mapValues = [mapInfo.name, mapInfo.latitude, mapInfo.longitude, mapInfo.map_id];
+  // let queryString = `UPDATE maps SET name = $1, latitude = $2, longitude = $3
+  // WHERE map_id = $4;
+  // VALUES ($1,$2,$3,$4) RETURNING *;`;
+  // return db
+  //   .query(queryString, mapValues)
+  //   .then(res => res.rows[0])
+  //   .catch(err => console.log(err));
+  let dbRes = [
+    {id: 3,
+      name: 'QUEBEC',
+      latitude: 50,
+      longitude: -90,
+      map_id:mapInfo.map_id
+    }
+  ];
+  return Promise.resolve(dbRes);
+
+};
+
+
+
+
 module.exports = {
   getAllUsers,
   getUserNameById,
-  getUserProfileById
+  getUserProfileById,
+  getPointsByMapId,
+  addMap,
+  editMap
 
 };
--- routes/maps.js
+++ routes/maps.js
@@ -10,17 +10,69 @@ const router  = express.Router();
 
 const mapsHelper = require('../db_service/db_helper');
 module.exports = (db) => {
-  // router.get("/", (req, res) => {
-  //   mapsHelper.getAllUsers(db)
-  //     .then(dbRes => {
-  //       res.json({ dbRes });
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
+  router.get("/:id", (req, res) => {
+    const mapId = req.params.id;
+
+
+
+
+    mapsHelper.getPointsByMapId(db, mapId)
+      .then(dbRes => {
+        res.json({ dbRes });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.patch("/:id", (req, res) => {
+    const mapId = req.params.id;
+
+    //get name, latitude , longtitude from front end
+    const name = req.body.name;
+    const latitude = req.body.latitude;
+    const longtitude = req.body.longtitude;
+    const mapInfo = {
+      name:name,
+      latitude:latitude,
+      longtitude:longtitude,
+      map_id:mapId
+    };
+
+    mapsHelper.editMap(db, mapInfo)
+      .then(dbRes => {
+        res.json({ dbRes });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/:id", (req, res) => {
+    const mapId = req.params.id;
+    //get name, latitude , longtitude from front end
+    const name = req.body.name;
+    const latitude = req.body.latitude;
+    const longtitude = req.body.longtitude;
+    const mapInfo = {
+      name:name,
+      latitude:latitude,
+      longtitude:longtitude
+    };
+    mapsHelper.addMap(db, mapInfo)
+      .then(dbRes => {
+        res.json({ dbRes });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
 
 
 
