54b811fb6e8d4e93ae91715a456306515025c195
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-12 01:19:48 -0500

    removed a backslash from user file

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 SELECT uuid_generate_v4();
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
-  id UUID DEFAULT uuid_generate_v4 () UNIQUE,\
+  id UUID DEFAULT uuid_generate_v4 () UNIQUE,
   admin BOOLEAN,
   name TEXT,
   description TEXT,
