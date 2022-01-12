51fb67f45fac5ef7a7dec603cf30054c8361a0fc
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-12 01:30:13 -0500

    minor corrections

--- db/queries/01_favourites_maps_by_user.sql
+++ db/queries/01_favourites_maps_by_user.sql
@@ -1,6 +1,6 @@
 SELECT users.id as user_id,
 users.name as username,
-maps.title as map,
+maps.title as map_title
 FROM users
 JOIN maps ON users.id = user_id
 JOIN favourites ON users.id = user_id;
