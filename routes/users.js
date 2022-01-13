be6009bae3ca64900ee1b7b2104ba05d85b7cd31
CUI <garycui222@gmail.com>
2022-01-13 17:08:55 -0500

    use route update

--- routes/users.js
+++ routes/users.js
@@ -59,8 +59,15 @@ module.exports = (db) => {
   });
 
   router.post('/:id/favourites', (req, res) => {
+    let userId = req.params.id;
     let dataObj = req.body;
-    userHelper.addUserFavouriteMap(db, dataObj).then((dbRes) => {
+
+    let newObj = {
+      userId: userId,
+      dataObj: dataObj,
+    };
+
+    userHelper.addUserFavouriteMap(db, newObj).then((dbRes) => {
       res.json(dbRes);
     });
   });
