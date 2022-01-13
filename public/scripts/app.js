a9c557b26d32cfc951cb2464fef984956c68edcf
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2022-01-13 21:37:38 +0000

    default Toronto

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 // Client facing scripts here
 let accessToken = 'pk.eyJ1IjoiZG1pdHJpeWF0c2Vua28iLCJhIjoiY2t5Ync3MWw2MGpqbTMxbWtzM3Zwa2F1MyJ9.PhF68nNuMvNvUcxEMRMNyg';
 
-var map = L.map('map').setView([43.7, -79.3], 13);
+var map = L.map('map').setView([43.65, -79.4], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
